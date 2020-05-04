22e04ca979e8f40e69526e1727e7c28267482361
Daniel Hernandez <00209317@uca.edu.sv>
2020-05-04 11:48:53 -0600

    finish

--- capas/src/main/java/com/uca/capas/capas/domain/Student.java
+++ capas/src/main/java/com/uca/capas/capas/domain/Student.java
@@ -1,4 +1,4 @@
-package com.uca.capas.domain;
+package com.uca.capas.capas.domain;
 
 public class Student {
 	private String name;
@@ -6,11 +6,12 @@ public class Student {
 	private String fEntrada;
 	private String carrera;
 	private Boolean estado;
-
+	
+	
+	
 	public Student() {
-
+		
 	}
-
 	public Student(String name, String apellido, String fEntrada, String carrera, Boolean estado) {
 		super();
 		this.name = name;
@@ -19,49 +20,39 @@ public class Student {
 		this.carrera = carrera;
 		this.estado = estado;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getApellido() {
 		return apellido;
 	}
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
 	public String getfEntrada() {
 		return fEntrada;
 	}
-
 	public void setfEntrada(String fEntrada) {
 		this.fEntrada = fEntrada;
 	}
-
 	public String getCarrera() {
 		return carrera;
 	}
-
 	public void setCarrera(String carrera) {
 		this.carrera = carrera;
 	}
-
 	public Boolean getEstado() {
 		return estado;
 	}
-
 	public void setEstado(Boolean estado) {
 		this.estado = estado;
 	}
-
+	
 	public String delegateEstado() {
-		return estado ? "Activo" : "Inactivo";
+		return estado ? "Activo":"Inactivo";
 	}
 
 }
